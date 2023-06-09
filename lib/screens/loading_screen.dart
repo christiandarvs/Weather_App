22a77fec10492c048bd48803f33e022e7a26b503
christiandarvs <christiandarvin4@gmail.com>
2023-06-10 01:50:22 +0800

    add changes

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/screens/weather_screen.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -20,26 +21,65 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
-  void getLocation() async {
+  Future<void> getLocation() async {
     Location location = Location();
+
     await location.getLocation();
     var response = await http.get(Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=057e14416124ad6da322d52d049ae9d7&units=metric'));
     var jsonData = jsonDecode(response.body);
+
     debugPrint('$jsonData');
     var temperature = jsonData['main']['temp'];
     var cityName = jsonData['name'];
     var countryName = jsonData['sys']['country'];
+    var weatherDesc = jsonData['weather'][0]['description'];
+    var condition = jsonData['weather'][0]['id'];
+    Weather weather = Weather(
+        countryName: countryName,
+        cityName: cityName,
+        weatherDesc: weatherDesc,
+        condition: condition,
+        temperature: temperature);
     await navigateToWeatherScreen(location.latitude, location.longitude,
-        cityName, countryName, temperature);
+        cityName, countryName, temperature, weather.getWeatherIcon(condition));
+
+    // sub-countries
+    // const apiKey = '057e14416124ad6da322d52d049ae9d7';
+    // final cities = ['Japan', 'Korea', 'Paris', 'Thailand', 'China'];
+
+    // for (final city in cities) {
+    //   final url =
+    //       'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+    //   final response = await http.get(Uri.parse(url));
+
+    //   if (response.statusCode == 200) {
+    //     final weatherJson = json.decode(response.body);
+    //     final weather = Weather(
+    //       cityName: city,
+    //       temperature: weatherJson['main']['temp'].toDouble(),
+    //       weatherDesc: weatherJson['weather'][0]['description'],
+    //     );
+    //   } else {
+    //     print('Failed to fetch weather data for $city');
+    //   }
+    //   await navigateToWeatherScreen(location.latitude, location.longitude,
+    //       cityName, countryName, temperature);
+    // }
   }
 
-  Future<void> navigateToWeatherScreen(double latitude, double longitude,
-      String cityName, String countryName, double temperature) async {
+  Future<void> navigateToWeatherScreen(
+      double latitude,
+      double longitude,
+      String cityName,
+      String countryName,
+      double temperature,
+      String weatherIcon) async {
     await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => WeatherScreen(
+          weatherIcon: weatherIcon,
           latitude: latitude,
           longitude: longitude,
           cityName: cityName,
--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -5,16 +5,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-
-import 'package:weather_app/services/country.dart';
+import 'package:weather_app/services/weather.dart';
 
 class WeatherScreen extends StatefulWidget {
   final double latitude;
   final double longitude;
   final String cityName;
+  final String weatherIcon;
   final double temperature;
   const WeatherScreen(
       {super.key,
+      required this.weatherIcon,
       required this.latitude,
       required this.longitude,
       required this.cityName,
@@ -25,6 +26,7 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  List<Weather> weatherData = [];
   late TextEditingController cityController;
   // String? cityName;
   // late double temperature;
@@ -36,189 +38,205 @@ class _WeatherScreenState extends State<WeatherScreen> {
   void initState() {
     super.initState();
     cityController = TextEditingController();
-    getLocation();
+    fetchWeatherData();
   }
 
-  Future getLocation() async {
-    // var response = await http.get(Uri.parse(
-    //     'https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=057e14416124ad6da322d52d049ae9d7&units=metric'));
-    // debugPrint(jsonDecode(response.body));
-    // return jsonDecode(response.body);
-    List countries = [
-      Country(cityName: 'Paris', weatherDesc: 'Sunny', temperature: 20),
-      Country(cityName: 'Japan', weatherDesc: 'Rainy', temperature: 18),
+  Future<void> fetchWeatherData() async {
+    const apiKey = '057e14416124ad6da322d52d049ae9d7';
+    final cities = [
+      'Paris',
+      'Seoul',
+      'Tokyo',
+      'Taipei',
+      'Singapore',
+      'Sydney',
+      'Manila'
     ];
-    final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=057e14416124ad6da322d52d049ae9d7&units=metric'));
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to load data');
+    for (final city in cities) {
+      final url =
+          'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final weatherJson = json.decode(response.body);
+        final weather = Weather(
+          condition: weatherJson['weather'][0]['id'],
+          cityName: city,
+          countryName: weatherJson['sys']['country'],
+          temperature: weatherJson['main']['temp'].toDouble(),
+          weatherDesc: weatherJson['weather'][0]['description'],
+        );
+
+        setState(() {
+          weatherData.add(weather);
+        });
+      } else {
+        print('Failed to fetch weather data for $city');
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: getLocation(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Scaffold(
-              resizeToAvoidBottomInset: false,
-              body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      SearchBar(
-                        hintText: 'Search City',
-                        side: const MaterialStatePropertyAll(
-                          BorderSide(color: Color(0xff6f7682)),
-                        ),
-                        backgroundColor:
-                            const MaterialStatePropertyAll(Colors.transparent),
-                        overlayColor: const MaterialStatePropertyAll(
-                          Color(0xff333742),
-                        ),
-                        shadowColor:
-                            const MaterialStatePropertyAll(Colors.transparent),
-                        controller: cityController,
-                        onChanged: (value) {
-                          setState(() {
-                            // cityName = value;
-                          });
-                        },
-                        leading: const Icon(
-                          Icons.search,
-                          color: Color(0xffe3e7e8),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        height: 130,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Card(
-                            color: const Color(0xff333742),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              SearchBar(
+                hintText: 'Search City',
+                side: const MaterialStatePropertyAll(
+                  BorderSide(color: Color(0xff6f7682)),
+                ),
+                backgroundColor:
+                    const MaterialStatePropertyAll(Colors.transparent),
+                overlayColor: const MaterialStatePropertyAll(
+                  Color(0xff333742),
+                ),
+                shadowColor: const MaterialStatePropertyAll(Colors.transparent),
+                controller: cityController,
+                onChanged: (value) {
+                  setState(() {
+                    // cityName = value;
+                  });
+                },
+                leading: const Icon(
+                  Icons.search,
+                  color: Color(0xffe3e7e8),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 130,
+                child: InkWell(
+                  onTap: () {},
+                  child: Card(
+                    color: const Color(0xff333742),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              widget.weatherIcon,
+                              style: const TextStyle(fontSize: 65),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
+                                Text(
+                                    '${widget.temperature.toStringAsFixed(0)}°C',
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold)),
                                 Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
                                   children: [
-                                    const Text(
-                                      '⛅',
-                                      style: TextStyle(fontSize: 65),
+                                    const Icon(Icons.location_pin),
+                                    const SizedBox(
+                                      width: 5,
                                     ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                            '${widget.temperature.toStringAsFixed(0)}°C',
-                                            style: GoogleFonts.poppins(
-                                                color: Colors.white,
-                                                fontSize: 25,
-                                                fontWeight: FontWeight.bold)),
-                                        Row(
-                                          children: [
-                                            const Icon(Icons.location_pin),
-                                            const SizedBox(
-                                              width: 5,
-                                            ),
-                                            Text(widget.cityName,
-                                                style: GoogleFonts.poppins(
-                                                    color: Colors.white,
-                                                    fontSize: 20)),
-                                          ],
-                                        )
-                                      ],
-                                    ),
+                                    Text(widget.cityName,
+                                        style: GoogleFonts.poppins(
+                                            color: Colors.white, fontSize: 20)),
                                   ],
-                                ),
+                                )
                               ],
                             ),
-                          ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Popular Locations'),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('View All'),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Expanded(
-                        child: ListView.separated(
-                          separatorBuilder: (context, index) {
-                            return const SizedBox(
-                              height: 5,
-                            );
-                          },
-                          itemCount: 5,
-                          itemBuilder: (context, index) {
-                            return SizedBox(
-                              height: 110,
-                              child: Card(
-                                color: const Color(0xff333742),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [
-                                            Text(
-                                                '${snapshot.data['main']['temp']}°C'),
-                                            const SizedBox(
-                                              width: 10,
-                                            ),
-                                            const Text('weatherDesc'),
-                                          ],
-                                        ),
-                                        Text(snapshot.data['name'])
-                                      ],
-                                    ),
-                                    const Text(
-                                      '⛅',
-                                      style: TextStyle(fontSize: 65),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            );
-          }
-          return const Scaffold();
-        });
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Popular Locations'),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('View All'),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 5,
+                    );
+                  },
+                  itemCount: weatherData.length,
+                  itemBuilder: (context, index) {
+                    final weather = weatherData[index];
+                    return SizedBox(
+                      height: 110,
+                      child: Card(
+                        color: const Color(0xff333742),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                        '${weather.temperature.toStringAsFixed(0)}°C',
+                                        style: GoogleFonts.poppins(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                        )),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                        '${weather.weatherDesc[0].toUpperCase()}${weather.weatherDesc.substring(1)}',
+                                        style: GoogleFonts.poppins(
+                                            color: Colors.white, fontSize: 16)),
+                                  ],
+                                ),
+                                Text(
+                                    '${weather.cityName}, ${weather.countryName}',
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white, fontSize: 16))
+                              ],
+                            ),
+                            Text(
+                              weather.getWeatherIcon(weather.condition),
+                              style: const TextStyle(fontSize: 50),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
